4f7d9bef4acb7e73c5a464ff6f75588469e0dbf1
Miillind Patro <millindpatro@outlook.com>
2026-05-27 19:54:51 +0530

    Update Region_analysis.py

--- pages/Region_analysis.py
+++ pages/Region_analysis.py
@@ -13,6 +13,14 @@ def read_excel(file):
     return df
 
 df = read_excel("data/sales.xls")
+
+
+if "Order Date" in df.columns:
+    df["Order Date"] = pd.to_datetime(df["Order Date"], errors="coerce")
+    df["Year"] = df["Order Date"].dt.year
+    df["Month"] = df["Order Date"].dt.month_name()
+    df["Month Number"] = df["Order Date"].dt.month
+    df["Year Month"] = df["Order Date"].dt.to_period("M").astype(str)
 st.subheader("Region Analysis")
 
 if "Region" in df.columns:
@@ -34,5 +42,25 @@ if "Region" in df.columns:
 
     st.plotly_chart(fig_region_sales, use_container_width=True)
 
+if "Year Month" in df.columns and "Region" in df.columns and "Sales" in df.columns:
+    region_trend = df.groupby(
+        ["Year Month", "Region"],
+        as_index=False
+    )["Sales"].sum()
+
+    fig5 = px.line(
+        region_trend,
+        x="Year Month",
+        y="Sales",
+        color="Region",
+        markers=True,
+        title="Region Wise Sales Trend"
+    )
+
+    st.plotly_chart(fig5, use_container_width=True)
+
+
     st.subheader("Region Table")
     st.dataframe(region_summary, use_container_width=True)
+
+
